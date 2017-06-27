b0764ba0a73ec3b36bcc7630715a88538dfc02bb
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-28 02:57:32 +1000

    Login Activity complete

--- app/src/main/java/com/community/mnahm5/clubsnade/LoginActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/LoginActivity.java
@@ -1,7 +1,15 @@
 package com.community.mnahm5.clubsnade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +18,32 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+    }
+
+    public void Login(View view) {
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        ParseUser.logInInBackground(
+                etUsername.getText().toString(),
+                etPassword.getText().toString(),
+                new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else if (e != null) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    public void RegisterLink(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
     }
 }
