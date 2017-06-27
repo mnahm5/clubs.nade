08d6362bd5fa48a8cbf8e81ce551879245d2fa45
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-28 01:26:51 +1000

    Database connection successful

--- app/src/main/java/com/community/mnahm5/clubsnade/StarterActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/StarterActivity.java
@@ -1,13 +1,41 @@
 package com.community.mnahm5.clubsnade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
 
 public class StarterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ParseSetup();
+
         setContentView(R.layout.activity_starter);
+    }
+
+    private void ParseSetup() {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("mnahm5")
+                .server("http://mnahm5-parse-server.herokuapp.com/parse/")
+                .build()
+        );
+
+        //ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
