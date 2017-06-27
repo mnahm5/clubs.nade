7d553b42b9d11b46ded077da230ff4b14cb09235
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-28 02:32:23 +1000

    Register Activity Complete

--- app/src/main/java/com/community/mnahm5/clubsnade/RegisterActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/RegisterActivity.java
@@ -1,7 +1,15 @@
 package com.community.mnahm5.clubsnade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +18,42 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setTitle("Registration");
+    }
+
+    public void Register(View view) {
+        final EditText etFullName = (EditText) findViewById(R.id.etFullName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPassword2 = (EditText) findViewById(R.id.etPassword2);
+
+        if (etPassword.getText().toString().equals(etPassword2.getText().toString())) {
+            ParseUser user = new ParseUser();
+            user.setUsername(etUsername.getText().toString());
+            user.setPassword(etPassword.getText().toString());
+            user.setEmail(etEmail.getText().toString());
+            user.put("fullName", etFullName.getText().toString());
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(getApplicationContext(), "User Account Created", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void Cancel(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
