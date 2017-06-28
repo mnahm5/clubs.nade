b28da1493b142a30c19a7d6d73471b00634002ee
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-29 01:06:17 +1000

    Gonna start working on payments

--- app/src/main/java/com/community/mnahm5/clubsnade/ClubDetailsActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/ClubDetailsActivity.java
@@ -1,17 +1,27 @@
 package com.community.mnahm5.clubsnade;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClubDetailsActivity extends AppCompatActivity {
@@ -40,11 +50,41 @@ public class ClubDetailsActivity extends AppCompatActivity {
                     tvClubDetails.setText(club.getString("details"));
                     String fees = tvFees.getText().toString() + " ";
                     tvFees.setText(fees.concat(club.getString("fees")));
+                    showClubLogo();
+                    checkMembership();
                 }
                 else if (e != null) {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    private void showClubLogo() {
+        final ImageView ivLogo = (ImageView) findViewById(R.id.ivLogo);
+
+        ParseFile file = (ParseFile) club.get("logo");
+        if (file != null) {
+            file.getDataInBackground(new GetDataCallback() {
+                @Override
+                public void done(byte[] data, ParseException e) {
+                    if (e == null && data != null) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        ivLogo.setImageBitmap(bitmap);
+                    }
+                }
+            });
+        }
+    }
+
+    private void checkMembership() {
+        final Button btJoin = (Button) findViewById(R.id.btJoin);
+
+        List<String> admins = club.getList("admins");
+        for (String userId: admins) {
+            if (userId.compareTo(ParseUser.getCurrentUser().getObjectId()) == 0) {
+                btJoin.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 }
