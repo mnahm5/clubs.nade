a42931521ed09cc475a0289e75c8685400b8ae61
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-28 19:57:06 +1000

    Can get images

--- app/src/main/java/com/community/mnahm5/clubsnade/CreateClubActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/CreateClubActivity.java
@@ -1,11 +1,19 @@
 package com.community.mnahm5.clubsnade;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.jar.Manifest;
 
 public class CreateClubActivity extends AppCompatActivity {
 
@@ -17,15 +25,51 @@ public class CreateClubActivity extends AppCompatActivity {
         setTitle("Create Club");
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhotos();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+                ImageView ivLogo = (ImageView) findViewById(R.id.ivLogo);
+                ivLogo.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
     public void ChooseImage(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivity(intent);
-
-
-        Toast.makeText(getApplicationContext(), "Pick your club logo", Toast.LENGTH_LONG).show();
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[] {android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        else {
+            getPhotos();
+        }
     }
 
     public void CreateClub(View view) {
         Toast.makeText(getApplicationContext(), "Club successfully created", Toast.LENGTH_LONG).show();
+    }
+
+    private void getPhotos() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
     }
 }
