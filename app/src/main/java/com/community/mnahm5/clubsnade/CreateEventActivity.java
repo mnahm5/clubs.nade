b1caba774e27bbcbf1d7a22366e778b977bdbea8
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-29 15:28:58 +1000

    Can choose image for event logo

--- app/src/main/java/com/community/mnahm5/clubsnade/CreateEventActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/CreateEventActivity.java
@@ -1,9 +1,16 @@
 package com.community.mnahm5.clubsnade;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -12,11 +19,13 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.io.IOException;
 import java.util.List;
 
 public class CreateEventActivity extends AppCompatActivity {
 
     private ParseObject club;
+    private Bitmap eventLogoBitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,36 @@ public class CreateEventActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spEventAccess.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhotos();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            try {
+                eventLogoBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+                ImageView ivEventLogo = (ImageView) findViewById(R.id.ivEventLogo);
+                ivEventLogo.setImageBitmap(eventLogoBitmap);
+                ivEventLogo.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     private void setup() {
@@ -54,5 +93,19 @@ public class CreateEventActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void ChooseEventLogo(View view) {
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[] {android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        else {
+            getPhotos();
+        }
+    }
+
+    private void getPhotos() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
     }
 }
