31e327c365752818a710df60e593eb82cd042b05
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-29 15:20:54 +1000

    Finished design for create event

--- app/src/main/java/com/community/mnahm5/clubsnade/CreateEventActivity.java
+++ app/src/main/java/com/community/mnahm5/clubsnade/CreateEventActivity.java
@@ -3,6 +3,8 @@ package com.community.mnahm5.clubsnade;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -22,6 +24,15 @@ public class CreateEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_event);
 
         setup();
+
+        Spinner spEventAccess = (Spinner) findViewById(R.id.spEventAccess);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.access_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spEventAccess.setAdapter(adapter);
     }
 
     private void setup() {
